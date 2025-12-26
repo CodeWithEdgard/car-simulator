12f88c2ff61e3d3245f72c1e7aec1c03c418e8c3
Edgar Mendes <mendesbr92@gmail.com>
2025-12-25 23:29:28 -0300

    feat: criado o service para acelerar o carro

---
+++ app/src/main/java/br/com/services/CarService.java
@@ -0,0 +1,18 @@
+package br.com.services;
+
+public interface CarService {
+
+    void acelerar();
+
+    void diminuirVelocidade();
+
+    void trocarMarcha();
+
+    void desligarCarro();
+
+    void virarEsquerda();
+
+    void virarDireita();
+
+
+}
---
+++ app/src/main/java/br/com/services/CarServicesImpl.java
@@ -0,0 +1,63 @@
+package br.com.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import br.com.domain.Car;
+import br.com.exception.CarDesligadoException;
+import br.com.exception.VelocidadeMaximaException;
+
+public class CarServicesImpl implements CarService {
+
+    private static final Logger Log = LoggerFactory.getLogger(CarServicesImpl.class);
+
+    private Car carro;
+
+    public CarServicesImpl(Car carro) {
+        this.carro = carro;
+    }
+
+    // Quando o carro for acelerado ele deve incrementar 1km em sua velocidade (pode chegar no
+    // máximo a 120km);
+    @Override
+    public void acelerar() {
+
+        if (!carro.getLigado()) {
+
+            throw new CarDesligadoException(
+                    "Carro esta desligado não e possivel fazer nenhuma ação");
+        }
+
+        if (carro.getVelocidade() >= 120) {
+            throw new VelocidadeMaximaException(
+                    "Não é possivel acelerar, alcançado  velocidade Maxima");
+        }
+
+        carro.setVelocidade(carro.getVelocidade() + 1);
+        Log.info("Voce aumentou a velocidade, estamos em: " + carro.getVelocidade());
+    }
+
+
+    @Override
+    public void diminuirVelocidade() {}
+
+
+
+    @Override
+    public void trocarMarcha() {}
+
+
+
+    @Override
+    public void desligarCarro() {}
+
+
+
+    @Override
+    public void virarEsquerda() {}
+
+    @Override
+    public void virarDireita() {}
+
+
+
+}
